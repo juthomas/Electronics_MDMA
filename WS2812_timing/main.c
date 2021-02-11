6a8405a407ca2a18e9af56f186401b1251563365
juthomas <juthomas@student.42.fr>
2021-02-11 23:13:04 +0100

    [ADD] beg assembly, but how to write PORTB ?

--- WS2812_timing/main.c
+++ WS2812_timing/main.c
@@ -2,8 +2,45 @@
 # define __AVR_ATmega328P__
 #endif
 #include "avr/io.h"
+//https://www.codeproject.com/Articles/15971/Using-Inline-Assembly-in-C-C
+//https://developer.arm.com/documentation/100748/0606/Using-Assembly-and-Intrinsics-in-C-or-C---Code/Writing-inline-assembly-code
+//https://stackoverflow.com/questions/36560646/r-vs-r-assembly-clarification
 
 int main()
 {
+
+	uint8_t red = 255;
+	uint8_t green = 255;
+	uint8_t blue = 255;
+	uint8_t registerLowMask = 0b00000000;//ancien registre + etat haut pin
+	uint8_t registerHighMask = 0b00000000;//ancien registre + etat bas pin
+	DDRB |= (1 << 0);
+	for (;;)
+	{
+		PORTB = 0b00000001;
+		PORTB = 0b00000000;
+
+		PORTB = 0b00000001;
+		PORTB = 0b00000000;
+
+		PORTB = 0b00000001;
+		PORTB = 0b00000000;
+
+		PORTB = 0b00000001;
+		PORTB = 0b00000000;
+
+		asm (
+			"mov %[high], %[low]\n"
+			: :
+			[red] "r" (red),
+			[green] "r" (green),
+			[blue] "r" (blue),
+			[low] "r" (registerLowMask),
+			[high] "r" (registerHighMask)
+			
+		);
+
+
+	}
 	return (0);
 }
