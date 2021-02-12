6d9e0c190fea300b0184ff02ddc781513ddcce6f
juthomas <juthomas@student.42.fr>
2021-02-12 15:54:15 +0100

    [ADD] multi pins testing

--- WS2812_timing/main.c
+++ WS2812_timing/main.c
@@ -12,14 +12,15 @@
 int main()
 {
 
-	uint8_t red = 255;
-	uint8_t green = 255;
-	uint8_t blue = 255;
-	uint8_t registerLowMask = 0b00000000;//ancien registre + etat haut pin
-	uint8_t registerHighMask = 0b00000001;//ancien registre + etat bas pin
-	uint8_t output = 0;
+	volatile uint8_t red = 255;
+	volatile uint8_t green = 255;
+	volatile uint8_t blue = 255;
+	volatile uint8_t registerLowMask = 0b00000000;//ancien registre + etat haut pin
+	volatile uint8_t registerHighMask = 0b00000011;//ancien registre + etat bas pin
+	volatile uint8_t output = 0;
 	cli();
 	DDRB |= (1 << 0);
+	DDRB |= (1 << 1);
 	for (;;)
 	{
 		PORTB = 0b00000001;//~= 0.33us
@@ -36,15 +37,24 @@ int main()
 
 		asm volatile(
 			// ".INCLUDE \"8515def.inc\"\n"
-			"out %[portb], %[low]  \n"//
-			"out %[portb], %[high] \n"//~= 0.08333us
-			"out %[portb], %[high] \n"//~= 0.08333us+0.08333us // fait un "bas haut" chelou
-			"out %[portb], %[low]  \n"//~= 0.1666us
-			"out %[portb], %[high] \n"
+			// "out %[portb], %[low]  \n"//
+			// "out %[portb], %[high] \n"//~= 0.08333us
+			// "out %[portb], %[high] \n"//~= 0.08333us+0.08333us // fait un "bas haut" chelou
+			// "out %[portb], %[high] \n"//~= 0.08333us+0.08333us // fait un "bas haut" chelou
+			// "out %[portb], %[high] \n"//~= 0.08333us+0.08333us // fait un "bas haut" chelou
+			// "out %[portb], %[high] \n"//~= 0.08333us+0.08333us // fait un "bas haut" chelou
+			// "out %[portb], %[low]  \n"//~= 0.1666us
+			// "out %[portb], %[high] \n"
+			"out %[portb], %[low]  \n"
 			"out %[portb], %[low]  \n"
 			"out %[portb], %[high] \n"
 			"out %[portb], %[low]  \n"
+			"out %[portb], %[low]  \n"
 			"out %[portb], %[high] \n"
+			"out %[portb], %[low]  \n"
+			"out %[portb], %[low]  \n"
+			"out %[portb], %[high] \n"
+			"out %[portb], %[low]  \n"
 			"out %[portb], %[low]  \n"
 			"out %[portb], %[high] \n"
 			"out %[portb], %[low]  \n"
