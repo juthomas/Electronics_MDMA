30a3376a6fc4955ca9b34de00ad73436bb636ee4
juthomas <juthomas@student.42.fr>
2021-02-24 11:49:34 +0100

    [ADD] main cleaned

--- Project/src/main.c
+++ Project/src/main.c
@@ -1,54 +1,11 @@
 #include "../inc/mdma.h"
 
-#define CPU_CLOCK 2000000
-
-void	wait_x_cpu_clocks(int32_t cpu_clocks)
-{
-	while (cpu_clocks > 0)
-	{
-		cpu_clocks-=3;
-	}
-}
-
-void	custom_delay(uint32_t milli)
-{
-	milli = milli *	2000;
-	wait_x_cpu_clocks(milli - 5);
-}
-
 int main()
 {
-	ft_pin_mode(FT_D11, FT_OUTPUT);
-	ft_pin_mode(FT_D13, FT_OUTPUT);
-	ft_pin_mode(FT_D14, FT_OUTPUT);
-	ft_pin_mode(FT_D30, FT_OUTPUT);
-	ft_pin_mode(FT_A3, FT_OUTPUT);
+	led_send_data();
 
 	for (;;)	
 	{
-		ft_digital_write(FT_D11, FT_LOW);
-		ft_digital_write(FT_D13, FT_HIGH);
-		ft_digital_write(FT_D14, FT_HIGH);
-		ft_digital_write(FT_D30, FT_LOW);
-		ft_digital_write(FT_A3, FT_LOW);
-		
-		custom_delay(10);
-		ft_digital_write(FT_D11, FT_HIGH);
-		ft_digital_write(FT_D13, FT_LOW);
-		ft_digital_write(FT_D14, FT_LOW);
-		ft_digital_write(FT_D30, FT_HIGH);
-		ft_digital_write(FT_A3, FT_HIGH);
-
-		custom_delay(10);
-		ft_digital_write(FT_D30, FT_LOW);
-		custom_delay(10);
-		ft_digital_write(FT_D30, FT_HIGH);
-		custom_delay(10);
-
-		ft_digital_write(FT_A3, FT_LOW);
-		custom_delay(10);
-		ft_digital_write(FT_A3, FT_HIGH);
-		custom_delay(10);
 	}
 	return (0);
 }
