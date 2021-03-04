c96f125954a37b451fca6024c6e12e430789c8c8
mgavillo <mgavillo@student.42.fr>
2021-03-04 12:37:39 +0100

    premier test de multiple bouton

--- Project/src/main.c
+++ Project/src/main.c
@@ -1,7 +1,10 @@
 #include "../inc/mdma.h"
 
 #define CPU_CLOCK 2000000
-
+#define CLKA 67
+#define DTA 69
+#define CLKB 65
+#define DTB 63
 // void	wait_x_cpu_clocks(int32_t cpu_clocks)
 // {
 // 	while (cpu_clocks > 0)
@@ -18,10 +21,42 @@
 
 int main()
 {
-	serial_test();
 
+	serial_init();
+	ft_pin_mode(CLKA, FT_INPUT);
+	ft_pin_mode(DTA, FT_INPUT);
+	ft_pin_mode(CLKB, FT_INPUT);
+	ft_pin_mode(DTB, FT_INPUT);
+int tmpa = ft_digital_read(CLKA);
+int tmpb = ft_digital_read(CLKB);
+int i = 0;
 	for (;;)	
 	{
+		int truc = ft_digital_read(CLKA);
+		if (truc != tmpa && truc == 1)
+		{
+			if(ft_digital_read(CLKA) == ft_digital_read(DTA))
+				serial_putstr("droite 3");
+			else
+				serial_putstr("gauche 3");
+serial_putstr("\n\r");
+
+		}
+		tmpa = truc;
+			truc = ft_digital_read(CLKB);
+		if (truc != tmpb && truc == 1)
+		{
+			if(ft_digital_read(CLKB) == ft_digital_read(DTB))
+				serial_putstr("droite 2 ");
+			else
+				serial_putstr("gauche 2");
+serial_putstr("\n\r");
+
+		}
+		tmpb = truc;
+
+		
+
 	}
 	return (0);
 }
