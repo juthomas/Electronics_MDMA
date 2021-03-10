223ac847dff0fd7e99f6aa2ca50547199c1b4b4d
mgavillo <mgavillo@student.42.fr>
2021-03-10 11:18:29 +0100

    test

--- Project/src/main.c
+++ Project/src/main.c
@@ -1,10 +1,17 @@
 #include "../inc/mdma.h"
 
+
 #define CPU_CLOCK 2000000
-#define CLKA 67
-#define DTA 69
-#define CLKB 65
-#define DTB 63
+#define DT 61
+#define CLK 21
+
+
+#define ENC_CTL	DDRB	//encoder port control
+#define ENC_WR	PORTB	//encoder port write	
+#define ENC_RD	PINB	//encoder port read
+#define ENC_A 0	
+#define ENC_B 1	
+
 // void	wait_x_cpu_clocks(int32_t cpu_clocks)
 // {
 // 	while (cpu_clocks > 0)
@@ -19,44 +26,35 @@
 // 	wait_x_cpu_clocks(milli - 5);
 // }
 
+//ma meilleur interupt
+ISR (INT0_vect){
+	serial_putnbr(ft_digital_read(CLK));
+	serial_putnbr(ft_digital_read(DT));
+	if(ft_digital_read(CLK) || ft_digital_read(DT))
+ 		serial_putstr("droite");
+	else
+		 serial_putstr("gauche");
+  serial_putstr(" id:0 \n\r");
+}
+
+//mon meilleur main
 int main()
 {
 
 	serial_init();
-	ft_pin_mode(CLKA, FT_INPUT);
-	ft_pin_mode(DTA, FT_INPUT);
-	ft_pin_mode(CLKB, FT_INPUT);
-	ft_pin_mode(DTB, FT_INPUT);
-int tmpa = ft_digital_read(CLKA);
-int tmpb = ft_digital_read(CLKB);
-int i = 0;
+
+//PCICR |= 0b10; 
+	EIMSK |= (1 << INT0); //autoriser interupt sur INT0
+	EICRA = (1 << ISC01); //interupt on higt
+	SREG = 0b10000000; //global interrupt enable
+
+
+	ft_pin_mode(CLK, FT_INPUT);
+	ft_pin_mode(DT, FT_INPUT);
 	for (;;)	
-	{
-		int truc = ft_digital_read(CLKA);
-		if (truc != tmpa && truc == 1)
-		{
-			if(ft_digital_read(CLKA) == ft_digital_read(DTA))
-				serial_putstr("droite 3");
-			else
-				serial_putstr("gauche 3");
-serial_putstr("\n\r");
-
-		}
-		tmpa = truc;
-			truc = ft_digital_read(CLKB);
-		if (truc != tmpb && truc == 1)
-		{
-			if(ft_digital_read(CLKB) == ft_digital_read(DTB))
-				serial_putstr("droite 2 ");
-			else
-				serial_putstr("gauche 2");
-serial_putstr("\n\r");
-
-		}
-		tmpb = truc;
-
-		
-
+	{		
 	}
+
+
 	return (0);
 }
